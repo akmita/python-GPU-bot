13f206392a1c25a004949bbb3cc09a90aa0f684a
Adrian Kmita <kmita111@gmail.com>
2021-07-04 14:54:40 -0500

    added auto login

--- bestbuy_bot_2.py
+++ bestbuy_bot_2.py
@@ -20,6 +20,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import threading
 import utils
+import os
 
 # card links
 AMD_results = "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&id=pcat17071&iht=y&keys=keys&ks=960&list=n&qp=currentprice_facet%3DPrice" \
@@ -33,8 +34,15 @@ NVIDIA_results = "https://www.bestbuy.com/site/searchpage.jsp?_dyncharset=UTF-8&
 PS5_results = "https://www.bestbuy.com/site/searchpage.jsp?st=playstation+5&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=" \
               "&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys"
 cart_page = "https://www.bestbuy.com/cart"
+
+#########################################
+# TODO _____ testing with PS5 results __
+LINK = NVIDIA_results
+#########################################
+
 # selectors
 numItemsInCart_selector = "dot"
+loginBtnXpath = "//button[@class='btn btn-secondary btn-lg btn-block c-button-icon c-button-icon-leading cia-form__controls__submit ']"
 # for performance measures
 startTime = time.time()*1000
 
@@ -63,6 +71,7 @@ def beginBuyingProcess(driver, wait, ATCButtons):
     startAlarm()
 
     print("waiting on you to finish buying process")
+    # TODO finish auto buying
     # wait till item in cart
     # wait.until(EC.presence_of_element_located((By.CLASS_NAME, "go-to-cart-button")))
     # myLogger("item in cart now")
@@ -72,6 +81,31 @@ def beginBuyingProcess(driver, wait, ATCButtons):
 
     # don't turn off the web driver
     WebDriverWait(driver, 9999999999).until(EC.presence_of_element_located((By.CLASS_NAME, "$$$$$$$$$$$$$$$$")))
+
+def handleLogin(driver, wait):
+    myLogger("checking if logged in")
+
+    # check if logged in
+    myAccountBtn = driver.find_elements_by_xpath("//span[@class='plButton-label v-ellipsis']")
+    if myAccountBtn[0].text == "Account":
+        print("  logging in")
+        driver.get("https://www.bestbuy.com/identity/global/signin")
+        # input email and password
+        wait.until(EC.element_to_be_clickable((By.XPATH, loginBtnXpath)))
+        print("  ready to login")
+        emailField = driver.find_element_by_id("fld-e")
+        passwordField = driver.find_element_by_id("fld-p1")
+        emailField.send_keys(os.environ.get("bb_username"))
+        passwordField.send_keys(os.environ.get("bb_password"))
+        # click login
+        [loginBtn] = driver.find_elements_by_xpath(loginBtnXpath)
+        loginBtn.click()
+        # wait till logged in (homepage footer loaded)
+        WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.CLASS_NAME, "footer-email-container")))
+        driver.get(LINK)
+        driver.implicitly_wait(8)
+    else:
+        myLogger("  already logged in !!")
 
 
 
@@ -87,15 +121,13 @@ def runBot():
 
     # go to search results
     myLogger("loading page DOUBLE CHECK THE LINK PLEASE")
-    # TODO _____ testing with PS5 results _____
-    driver.get(NVIDIA_results)
-    # driver.get(PS5_results)
+    driver.get(LINK)
 
-    try:
-        #  TODO remove wait once we develop auto login
-        WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME, "$$$$$$$$$$$$$$$$")))
-    except:
-        print("continueing")
+    #  TODO remove wait once we develop auto login
+    # try:
+    #     WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.CLASS_NAME, "$$$$$$$$$$$$$$$$")))
+    # except:
+    #     print("continueing")
 
 
     # query for add to cart buttons
@@ -105,27 +137,18 @@ def runBot():
 
     # if buttons were found, click one, ifn ot refresh and repeat
     while True:
+        print("-------------------------------------")
+        handleLogin(driver, wait)
+
         if ATCButtons:
             myLogger("number of found buttons: ")
             print(len(ATCButtons))
             beginBuyingProcess(driver, wait, ATCButtons)
         else:
             myLogger("nothing found... refreshing")
-            driver.refresh()
+            driver.get(LINK)
             myLogger("finished refresh")
             resetTimer()
-
-        # TODO MANUALLY login (for now)
-        # TODO MANUALLY add credit card info on best buy
-
-
-
-    # TODO TEST on PS5 page
-    # TODO OPTIMIZATION
-    #   maybe use beautifulsoup to query things faster
-
-def runBot2():
-    pass
 
 
 ############################
